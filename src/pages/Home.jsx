bf2a22241bd3fb7ed25ecc2c9c327232c1284a23
nguyenhuyennd <nguyenhuyennd1211@gmail.com>
2021-11-10 19:04:15 +0700

    create HomePage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,16 @@
+import { Link } from "react-router-dom"
+
 import Helmet from "../components/Helmet"
 import HeroSlider from "../components/HeroSlider"
+import Section, { SectionTitle, SectionBody } from "../components/Section"
+import PolicyCard from "../components/PolicyCard"
+import Grid from "../components/Grid"
+
 import heroSliderData from "../assets/fake-data/hero-slider"
+import policy from "../assets/fake-data/policy"
+import productData from "../assets/fake-data/products.js"
+import ProductCard  from "../components/ProductCard"
+
 const Home = () => {
     return (
         <Helmet title="Trang chủ">
@@ -11,6 +21,91 @@ const Home = () => {
                 auto={true}
             />
             {/* end hero slider */}
+            {/* Policy Section */}
+            <Section>
+                <SectionBody>
+                    <Grid
+                        col={4}
+                        mdCol={2}
+                        smCol={1}
+                        gap={20}
+
+                    >
+                    {
+                        policy.map((item, index) => (
+                            <Link to="/policy" key={index}>
+                                <PolicyCard
+                                    name={item.name}
+                                    description={item.description}
+                                    icon={item.icon}
+                                />
+                            </Link>
+                        ))
+                    }
+                    </Grid>
+                </SectionBody>
+            </Section>
+            {/* End Policy Section */}
+            {/* best selling */}
+            <Section>
+                <SectionTitle>
+                    Top săn phẩm bán chạy trong tuần
+                </SectionTitle>
+                <SectionBody>
+                    <Grid
+                        col={4}
+                        mdCol={2}
+                        smCol={1}
+                        gap={20}
+
+                    >
+                        {
+                            productData.getProducts(4).map((item, index) => (
+                                <ProductCard
+                                    key={index}
+                                    img01={item.image01}
+                                    img02={item.image02}
+                                    slug={item.slug}
+                                    name={item.title}
+                                    price={+item.price}
+
+                                />
+                            ))
+                        }
+                    </Grid>
+                </SectionBody>
+            </Section>
+            {/* end best selling */}
+            {/* new arrival section */}
+            <Section>
+                <SectionTitle>
+                    Sản phẩm mới
+                </SectionTitle>
+                <SectionBody>
+                    <Grid
+                        col={4}
+                        mdCol={2}
+                        smCol={1}
+                        gap={20}
+
+                    >
+                        {
+                            productData.getProducts(8).map((item, index) => (
+                                <ProductCard
+                                    key={index}
+                                    img01={item.image01}
+                                    img02={item.image02}
+                                    slug={item.slug}
+                                    name={item.title}
+                                    price={+item.price}
+
+                                />
+                            ))
+                        }
+                    </Grid>
+                </SectionBody>
+            </Section>
+            {/* new new arrival section */}
         </Helmet>
     )
 }
---
+++ src/utils/numberWithCommas.js
@@ -0,0 +1,2 @@
+const numberWithCommas = (num) => num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+export default numberWithCommas
