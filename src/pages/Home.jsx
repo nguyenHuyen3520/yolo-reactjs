40c9cc2c9ae1d44d7a1aba9199e7f3222f0418a8
nguyenhuyennd <nguyenhuyennd1211@gmail.com>
2021-11-10 19:20:57 +0700

    HomePage: banner, popular product

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,9 @@ import Grid from "../components/Grid"
 import heroSliderData from "../assets/fake-data/hero-slider"
 import policy from "../assets/fake-data/policy"
 import productData from "../assets/fake-data/products.js"
-import ProductCard  from "../components/ProductCard"
+import ProductCard from "../components/ProductCard"
+
+import banner from "../assets/images/banner.png"
 
 const Home = () => {
     return (
@@ -106,6 +108,45 @@ const Home = () => {
                 </SectionBody>
             </Section>
             {/* new new arrival section */}
+            {/* Banner */}
+            <Section>
+                <SectionBody>
+                    <Link to="/catalog">
+                        <img src={banner} alt="" />
+                    </Link>
+                </SectionBody>
+            </Section>
+            {/* end Banner */}
+            {/* popular products  */}
+            <Section>
+                <SectionTitle>
+                    Sản phẩm phổ biến
+                </SectionTitle>
+                <SectionBody>
+                    <Grid
+                        col={4}
+                        mdCol={2}
+                        smCol={1}
+                        gap={20}
+
+                    >
+                        {
+                            productData.getProducts(12).map((item, index) => (
+                                <ProductCard
+                                    key={index}
+                                    img01={item.image01}
+                                    img02={item.image02}
+                                    slug={item.slug}
+                                    name={item.title}
+                                    price={+item.price}
+
+                                />
+                            ))
+                        }
+                    </Grid>
+                </SectionBody>
+            </Section>
+            {/* end popular products  */}
         </Helmet>
     )
 }
